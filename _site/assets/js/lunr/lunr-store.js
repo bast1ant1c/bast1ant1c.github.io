d76aeeae88907dbc6fb351360560f2101586e11d
bast1ant1c <bast1ant1c@localhost.localdomain>
2022-03-23 00:20:24 -0500

    article

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Un post bien Hack",
-        "excerpt":"Artículo Hack!  ","categories": [],
-        "tags": ["Pentesting"],
-        "url": "http://localhost:4000/post-hack/",
-        "teaser":null}]
+        "title": "eJPT Review",
+        "excerpt":"¡Hola! Vengo a contar mi experiencia de como logré adquirir el certificado de “eLearnSecurity Junior Penetration Tester” (eJPT) He dividido en 6 secciones lo que pude identificar, para cada una de las fases de mi preparación antes, durante y después del examen. Espero le sirva a la persona que está...","categories": ["Certificaciones"],
+        "tags": ["Pentesting","eLearnSecurity"],
+        "url": "http://localhost:4000/ejpt/",
+        "teaser":"http://localhost:4000/assets/images/ejpt/ejpt.png"}]
