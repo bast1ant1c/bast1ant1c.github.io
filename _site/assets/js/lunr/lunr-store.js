1ebd091d9ebc7ca5287e95e28410618fe2fb6c41
root <root@localhost.localdomain>
2022-04-11 20:55:08 -0500

    article

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"¡Hola! Vengo a contar mi experiencia de como logré adquirir el certificado de “eLearnSecurity Junior Penetration Tester” (eJPT) He dividido en 6 secciones lo que pude identificar, para cada una de las fases de mi preparación antes, durante y después del examen. Espero le sirva a la persona que está...","categories": ["Certificaciones"],
         "tags": ["Pentesting","eLearnSecurity"],
         "url": "http://localhost:4000/ejpt/",
-        "teaser":"http://localhost:4000/assets/images/ejpt/ejpt.png"}]
+        "teaser":"http://localhost:4000/assets/images/ejpt/ejpt.png"},{
+        "title": "THM Spring4Shell",
+        "excerpt":"¡Hola! En esta ocasión vamos a hablar de la maquina Spring4shell. TryHackMe lanzó hace unas semanas esta máquina para aprender acerca de una de las vulnerabilidades más sonadas del momento. El impacto de la vulnerabilidad asciende al nivel de que, piratas informáticos sacaron provecho de este acontecimiento para ejecutar el...","categories": ["TryHackMe"],
+        "tags": ["Writeup","Spring4shell"],
+        "url": "http://localhost:4000/spring4shell/",
+        "teaser":"http://localhost:4000/assets/images/spring4shell/inicio.png"}]
