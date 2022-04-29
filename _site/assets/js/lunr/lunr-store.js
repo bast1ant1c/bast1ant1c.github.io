1bbc8bbfc809bd853d2f5d980a788d6b0c932409
root <root@localhost.localdomain>
2022-04-28 23:59:28 -0500

    article

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"¡Hola! En esta ocasión vamos a hablar de la maquina Spring4shell. TryHackMe lanzó hace unas semanas esta máquina para aprender acerca de una de las vulnerabilidades más sonadas del momento. El impacto de la vulnerabilidad asciende al nivel de que, piratas informáticos sacaron provecho de este acontecimiento para ejecutar el...","categories": ["TryHackMe"],
         "tags": ["Writeup","Spring4shell"],
         "url": "http://localhost:4000/spring4shell/",
-        "teaser":"http://localhost:4000/assets/images/spring4shell/inicio.png"}]
+        "teaser":"http://localhost:4000/assets/images/spring4shell/inicio.png"},{
+        "title": "HTB Knife",
+        "excerpt":"¡Hola! En esta ocasión vamos a resolver de la maquina Knife de HackTheBox. La maquina es nivel “Easy”, sin embargo, el nivel siempre se lo pones tú, al enfrentar estos retos, ¡vamos a ponernos hack! PREPARACIÓN Para iniciar nuestra máquina, vamos a crear un directorio de trabajo con el nombre...","categories": ["HackTheBox"],
+        "tags": ["Writeup","Knife"],
+        "url": "http://localhost:4000/knife/",
+        "teaser":"http://localhost:4000/assets/images/knife/01inicio.png"}]
